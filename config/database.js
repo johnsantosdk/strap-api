054448ad21729e621233edfa1f6ceb4967b3732c
Airton Santos <airton.santos3.1415@gmail.com>
2021-04-10 10:52:42 -0300

    turn ON SSL on postgres

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME', 'blog_strapi_api'),
         username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'toor'),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: env.bool('DATABASE_SSL', true),
       },
       options: {}
     },
